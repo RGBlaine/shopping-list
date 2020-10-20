a846a2a0749a3599cd640fc37713fc3b58e64ff6
Robin G. Blaine <rgblaine70@gmail.com>
2020-10-19 20:42:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,33 @@
-$(function () {
-    $('#js-shopping-list-form').submit(event => {
-
-        event.preventDefault();
-
-        let newItemName = ($(event.currentTarget).find(
-            'input[name="shopping-list-entry"]').val());
-
-        let newListItem =
-            `<li>
-                <span class="shopping-item">${newItemName}</span>
-                <div class="shopping-item-controls">
-                    <button class="shopping-item-toggle">
-                     <span class="button-label">check</span>
-                    </button>
-                        <button class="shopping-item-delete">
-                            <span class="button-label">delete</span>
-                    </button>
-                </div>
-            </li>`;
-
-        console.log(this);
-        $('.shopping-list').append(newListItem);
-
+$(function(){
+    $('#js-shopping-list-form').submit(function(event) {
+      event.preventDefault();
+      let newItem = ($(event.currentTarget).find(
+        'input[name="shopping-list-entry"]').val());
+  
+      $('#shopping-list-entry').val('');
+  
+  
+      $('.shopping-list').append(
+        `<li>
+          <span class="shopping-item">${newItem}</span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>`);
     });
-
-    $('#shopping-list')
-
-});
-
-
-$('.shopping-item-toggle').on('click', (function (event) {
-
-    event.stopPropagation();
-
-    console.log(this);
-    $(this.closest('li').children[0]).toggleClass('shopping-item__checked');
-
-}))
-
-
-$('.shopping-item-delete').on('click', (function (event) {
-
-    event.stopPropagation();
-
-    console.log(this.closest('li'));
-    this.closest('li').remove();
-
-}))
+  
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+      $(this).closest('li').remove();
+    });
+  
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+      $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
+  
+  });
 
